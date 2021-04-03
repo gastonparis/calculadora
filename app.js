f2bcf936ef00ad943e0b757af5903c1accd6d524
gastonparis <gastonparis@gmail.com>
2021-04-03 09:06:05 -0300

    agregue app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA8mFJP6XWhxYC5b_wSwsMbYQck_wHWjDo",
+    authDomain: "calculadora-c6d1f.firebaseapp.com",
+    projectId: "calculadora-c6d1f",
+    storageBucket: "calculadora-c6d1f.appspot.com",
+    messagingSenderId: "882449623031",
+    appId: "1:882449623031:web:b01a6b398304004c9330f2"
+  };
+  // Initialize Firebase
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  //var ui = new firebaseui.auth.AuthUI(firebase.auth());
